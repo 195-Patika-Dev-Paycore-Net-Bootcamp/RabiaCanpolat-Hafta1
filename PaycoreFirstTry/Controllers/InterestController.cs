184ec4c6e7fcd806aeb372de8f37574412150084
rcanpolaat <rcanpolaat@gmail.com>
2022-08-13 11:28:47 +0300

    Add project files.

---
+++ PaycoreFirstTry/Controllers/InterestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaycoreFirstTry.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult CalculateCompoundInterest(double total, double interestrate, double time)
+        {
+            if(total < 0 || interestrate < 0 || time < 0)
+            {
+                return BadRequest();
+            }
+            InterestResponse interestResponse = new InterestResponse();
+            interestResponse.TotalBalance = total * Math.Pow((1 + (interestrate / 100)), time);
+            interestResponse.InterestAmount = interestResponse.TotalBalance - total;
+            return Ok(interestResponse);
+        }
+    }
+}
---
+++ PaycoreFirstTry/InterestResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PaycoreFirstTry
+{
+    public class InterestResponse
+    {
+        public double InterestAmount { get; set; } //Faiz Oranı
+        public double TotalBalance { get; set; } //Toplam Para
+    }
+}
